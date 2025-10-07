9774cb9bceeb7b7243d3f0b6ec7e9c26db67409b
PedroHMantis <gtx1050pd@gmail.com>
2025-10-07 13:27:19 -0300

    Main view controller

--- src/main/java/org/mantisph/workshopjavafx/Main.java
+++ src/main/java/org/mantisph/workshopjavafx/Main.java
@@ -8,10 +8,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
         ScrollPane scrollPane = fxmlLoader.load();
 
         scrollPane.setFitToHeight(true);
